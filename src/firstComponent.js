02934afbc36e24827d3e94572dfccc2b1102be41
pydev97@gmail.com <pydev97@gmail.com>
2021-05-19 13:33:00 +0700

    add

--- src/firstComponent.js
+++ src/firstComponent.js
@@ -1,32 +1,27 @@
-import React from "react";
-export default class FirstComponent extends React.Component {
-  //make state
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-    };
-  }
-  componentDidMount() {
-    // khi compoent đã được insert vào Dom
-    console.log("component đã được insert vào dom");
-    this.timeId = setInterval(() => this.tick(), 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timeId);
-  }
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <h2>It is {this.timeId}.</h2>
-      </div>
-    );
-  }
+import React, { useState } from "react";
+export default function FirstComponent() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <label for="userName">User name:</label>
+      <input
+        type="text"
+        id="userName"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <br></br>
+      <label for="password">Password:</label>
+      <input
+        type="text"
+        id="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <br></br>
+      <p>{name}</p>
+      <button>Login</button>
+    </div>
+  );
 }
